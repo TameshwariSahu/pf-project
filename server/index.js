05b07823d17bf0bcd86e37cff8e04159d5b799c4
Tameshwari Sahu <tameshwarisahu961@gmail.com>
2026-04-16 16:32:25 +0530

    add netlify domain in cors

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+     "https://pf-statement.netlify.app",
     "https://pf-project-backend.onrender.com"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
