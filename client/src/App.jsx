fa19488c0f09abd09ff1fa18b780cfd175b51b2d
Tameshwari Sahu <tameshwarisahu961@gmail.com>
2026-04-22 10:51:36 +0530

    Reduced gap between login buttons and form UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -30,7 +30,7 @@ function App() {
           
 <div className="min-h-screen flex flex-col items-center bg-gradient-to-r from-purple-400 to-blue-400 pt-2">
                 
-               <div className="mb-8 flex gap-4 justify-center mt-2">
+              <div className="mb-3 flex gap-4 justify-center mt-2">
                   <button
                     onClick={() => setLoginType("normal")}
                     className={`px-6 py-2 rounded-full font-semibold ${
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -73,9 +73,9 @@ function Login({ setUser, isFinance }) {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-purple-500 via-blue-500 to-indigo-600 px-2">
+    <div className="flex items-center justify-center bg-gradient-to-br from-purple-500 via-blue-500 to-indigo-600 px-2">
       
-      <div className="bg-white w-full max-w-md rounded-2xl shadow-2xl p-4">
+      <div className="bg-white w-full max-w-md rounded-2xl shadow-2xl p-8">
         
         <h2 className="text-3xl font-bold text-center text-gray-800 mb-6">
           {isFinance ? "Finance Login" : "User Login"}
