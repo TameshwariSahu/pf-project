2b4356a9ecdbab8296606b85584fbfa5f9917c24
Tameshwari Sahu <tameshwarisahu961@gmail.com>
2026-04-23 10:03:07 +0530

    no data alert is added in pfstatement

--- client/src/components/PFStatement.jsx
+++ client/src/components/PFStatement.jsx
@@ -162,7 +162,7 @@ useEffect(() => {
       toast.success("Data fetched ✅");
     } catch (err) {
       console.log(err);
-      toast.error("Error fetching ❌");
+      toast.error("NO data ❌");
     }
   };
 
