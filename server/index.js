183995088d7d01b58e82c39a7aa04279a736e347
Tameshwari Sahu <tameshwarisahu961@gmail.com>
2026-04-24 12:27:03 +0530

    feat: add /ping route to keep render server alive

--- server/index.js
+++ server/index.js
@@ -61,6 +61,7 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+app.get("/ping", (req, res) => res.send("pong"));
 app.listen(PORT, () => {
   console.log("Server running on", PORT);
 });
